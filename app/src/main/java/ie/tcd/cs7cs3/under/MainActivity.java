c8b57f05a80a055e8aaae44e260ff9601868b057
Cian Johnston <public@cianjohnston.ie>
2019-11-05 11:57:57 +0000

    initial commit

---
+++ app/src/main/java/ie/tcd/cs7cs3/under/MainActivity.java
@@ -0,0 +1,14 @@
+package ie.tcd.cs7cs3.under;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
