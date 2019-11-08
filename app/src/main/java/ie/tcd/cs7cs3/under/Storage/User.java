2c7b587d31be83cc53c7777267b2ceb55670718b
chavvichandani <chavvi96@gmail.com>
2019-11-08 10:30:47 +0000

    Added User class Updated build.gradle

---
+++ app/src/main/java/ie/tcd/cs7cs3/under/Storage/User.java
@@ -0,0 +1,55 @@
+package ie.tcd.cs7cs3.under.Storage;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    private final long id;
+    private String name;
+    private int age;
+    private String address;
+    private String gender;
+
+    public User(long id, String name, int age, String gender, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
