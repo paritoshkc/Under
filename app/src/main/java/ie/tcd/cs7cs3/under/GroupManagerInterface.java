ec2be211692dd7494f594c80a718d560ea590571
sspirkl <spirkl@logos-gmbh.com>
2019-11-12 11:13:10 +0000

    adds interface for Group

--- app/src/main/java/ie/tcd/cs7cs3/under/GroupManagerInterface.java
+++ app/src/main/java/ie/tcd/cs7cs3/under/GroupManagerInterface.java
@@ -6,12 +6,12 @@
 package ie.tcd.cs7cs3.under;
 import java.util.List;
 
-public interface GroupManager {
-        /** Received
+public interface GroupManagerInterface {
+        /** Capsules all group LISTS related activities.
         * @author Stefan Spirkl
         * @param latitude / the latitude of the destination we want to find groups for
         * @param longitude / the latitude of the destination we want to find groups for
         * @return a list of groups that fit that destination / have a a similar destination
         */
-        List<Group> findSimilarDestinationGroups(float latitude, float longitude);
+        List<Group> findSimilarDestinationGroups(final float latitude, final float longitude);
 }
