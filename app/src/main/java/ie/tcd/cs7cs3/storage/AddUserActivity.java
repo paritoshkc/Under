9a27ef591ec8d7b858bf1384a8d270163f0369b8
Cian Johnston <public@cianjohnston.ie>
2019-11-21 16:35:53 +0000

    storage: rename StorageActivity -> AddUserActivity

--- app/src/main/java/ie/tcd/cs7cs3/storage/AddUserActivity.java
+++ app/src/main/java/ie/tcd/cs7cs3/storage/AddUserActivity.java
@@ -1,24 +1,18 @@
 package ie.tcd.cs7cs3.storage;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import ie.tcd.cs7cs3.under.R;
 
-public class StorageActivity extends AppCompatActivity {
+public class AddUserActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_storage);
+    setContentView(R.layout.activity_add_user);
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
