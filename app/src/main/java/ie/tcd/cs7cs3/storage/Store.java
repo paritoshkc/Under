8cceec459b12696a3f44caeabd7b7ee8949ca95e
Cian Johnston <public@cianjohnston.ie>
2019-11-21 18:25:53 +0000

    Storage: add Store implementation

---
+++ app/src/main/java/ie/tcd/cs7cs3/storage/Store.java
@@ -0,0 +1,29 @@
+package ie.tcd.cs7cs3.storage;
+
+import ie.tcd.cs7cs3.under.GroupManager.UserInterface;
+import java.util.Optional;
+
+public class Store implements StoreInterface {
+  private final UserEntityDAO userEntityDAO;
+  private final UserRatingEntityDAO userRatingEntityDAO;
+
+  public Store(final AppDatabase appDatabase) {
+    this.userEntityDAO = appDatabase.getUserEntityDao();
+    this.userRatingEntityDAO = appDatabase.getUserRatingEntityDao();
+  }
+
+  @Override
+  public UserInterface getCurrentUser() {
+    return null;
+  }
+
+  @Override
+  public Optional<UserInterface> getUser(String uuid) {
+    return Optional.empty();
+  }
+
+  @Override
+  public void rateUser(UserInterface from, int rating) {
+
+  }
+}
