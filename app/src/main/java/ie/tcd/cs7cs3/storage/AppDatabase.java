1af5d75ce469a6c6acd462d94af6652735e2ccba
Cian Johnston <public@cianjohnston.ie>
2019-11-22 12:22:24 +0000

    AppDatabase: add convenience method for getting a temp in-memory database

--- app/src/main/java/ie/tcd/cs7cs3/storage/AppDatabase.java
+++ app/src/main/java/ie/tcd/cs7cs3/storage/AppDatabase.java
@@ -1,12 +1,27 @@
 package ie.tcd.cs7cs3.storage;
 
+import android.content.Context;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(
     entities = {UserEntity.class, UserRatingEntity.class},
     version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+  private static AppDatabase INSTANCE;
+
   public abstract UserEntityDAO getUserEntityDao();
   public abstract UserRatingEntityDAO getUserRatingEntityDao();
+
+  public static AppDatabase getInMemoryAppDatabase(Context context) {
+    if (null != INSTANCE) {
+      return INSTANCE;
+    }
+    // inmemory database builder for now
+    INSTANCE = Room.inMemoryDatabaseBuilder(context.getApplicationContext(), AppDatabase.class)
+        .allowMainThreadQueries() // XXX: this is bad
+        .build();
+    return INSTANCE;
+  }
 }
