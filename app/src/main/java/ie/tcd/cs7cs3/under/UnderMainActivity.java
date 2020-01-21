9ec812627a58bd508015e76907baad3c92afddd6
paritoshkc <paritoshkchauhan@gmail.com>
2020-01-21 14:12:40 +0000

    Adding missing Code for Module specific buttons

--- app/src/main/java/ie/tcd/cs7cs3/under/UnderMainActivity.java
+++ app/src/main/java/ie/tcd/cs7cs3/under/UnderMainActivity.java
@@ -7,6 +7,9 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import ie.tcd.cs7cs3.under.GroupManager.MainActivity;
+import ie.tcd.cs7cs3.under.locationmanager.Navigation;
+import ie.tcd.cs7cs3.under.storage.ListUsersActivity;
 import ie.tcd.cs7cs3.under.wifiP2P.WifiMainActivity;
 
 public class UnderMainActivity extends AppCompatActivity {
@@ -17,10 +20,7 @@ public class UnderMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_under_activity);
         init();
-        //exqListener();
-
     }
-
 
     private void init() {
         btnWifiP2p=(Button) findViewById(R.id.wifiP2p);
@@ -32,31 +32,31 @@ public class UnderMainActivity extends AppCompatActivity {
                 view.getContext().startActivity(intent);}
         });
 
+        btnUserLocation=(Button) findViewById(R.id.userLocation);
 
-//        btnOnOff=(Button) findViewById(R.id.onOff);
-//        btnOnOff=(Button) findViewById(R.id.onOff);
-//        btnOnOff=(Button) findViewById(R.id.onOff);
+        btnUserLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Navigation.class);
+                view.getContext().startActivity(intent);}
+        });
 
+        btnDataStorage=(Button) findViewById(R.id.dataStorage);
 
+        btnDataStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ListUsersActivity.class);
+                view.getContext().startActivity(intent);}
+        });
 
+        btnGroupManager = (Button) findViewById(R.id.groupManager );
 
-//        btnDiscover=(Button) findViewById(R.id.discover);
-//        btnSend=(Button) findViewById(R.id.sendButton);
-//        listView=(ListView) findViewById(R.id.peerListView);
-//        read_msg_box=(TextView) findViewById(R.id.readMsg);
-//        WriteMsg=(EditText) findViewById(R.id.writeMsg);
-//        connectionStatus = (TextView) findViewById(R.id.connectionStatus);
-//
-//        wifiManager= (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-//        mManager= (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-//        mChannel=mManager.initialize(this, getMainLooper(),null);
-//
-//        mReceiver=new WifiDirectBroadcastReceiver(mManager, mChannel, this);
-//
-//        mIntentFilter=new IntentFilter();
-//        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-//        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-//        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-//        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        btnGroupManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                view.getContext().startActivity(intent);}
+        });
     }
 }
